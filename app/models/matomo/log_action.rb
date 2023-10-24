1d661b1fe055ab2b5b6a09a090c7d03f65fffa74
KeisukeKitazumi <kitazumi@sikmi.com>
2023-10-24 15:01:54 +0900

    カラム名`hash`でエラーが出るのを回避

--- app/models/matomo/log_action.rb
+++ app/models/matomo/log_action.rb
@@ -12,5 +12,10 @@ module Matomo
     has_many :log_link_visit_actions_category, class_name: 'Matomo::LogLinkVisitAction',
                                                foreign_key: 'idaction_event_category'
 
+    def self.instance_method_already_implemented?(method_name)
+      return true if method_name == 'hash'
+
+      super(method_name)
+    end
   end
 end
