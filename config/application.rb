3364320d3bd9350bf3c2c8b81f56b114cb5a3e91
KeisukeKitazumi <kitazumi@sikmi.com>
2023-10-25 10:40:32 +0900

    install slim

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,7 @@ module App
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.generators.template_engine = :slim
   end
 end
