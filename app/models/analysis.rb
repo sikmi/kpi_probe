6cba2f83e0c759fec47d531ef6b96c92a32874d7
KeisukeKitazumi <kitazumi@sikmi.com>
2023-11-06 16:48:55 +0900

    startのログを基点にするように変更

--- app/models/analysis.rb
+++ app/models/analysis.rb
@@ -10,21 +10,22 @@ class Analysis
 
   def self.search(search_params)
     categories = Matomo::Action.where(type: TYPE_EVENT_CATEGORY).where.not('name LIKE ?', '%/%').search_process_name(search_params[:process_name])
-    finish_logs = Matomo::LinkVisitAction.finish_logs(categories).order(server_time: :desc).search_user_name(search_params[:user_name]).serarch_period(
+    start_logs = Matomo::LinkVisitAction.start_logs(categories).order(server_time: :desc).search_user_name(search_params[:user_name]).serarch_period(
       search_params[:start_date]&.to_date || DEFAULT_START_DATE, search_params[:end_date]&.to_date || Time.zone.today
     )
 
-    build_analysises(finish_logs)
+    build_analysises(start_logs)
   end
 
-  def self.build_analysises(finish_logs)
-    finish_logs.map do |finish_log|
-      start_log = Matomo::LinkVisitAction.start_log(finish_log)
+  def self.build_analysises(start_logs)
+    start_logs.map do |start_log|
+      finish_log = Matomo::LinkVisitAction.finish_log(start_log)
+
       Analysis.new(
         started_at: start_log.server_time,
-        process_name: finish_log.event_category.name,
-        user_name: finish_log.visit.user.name,
-        time: Time.at(finish_log.server_time - start_log.server_time).utc.strftime('%H:%M:%S')
+        process_name: start_log.event_category.name,
+        user_name: start_log.visit.user.name,
+        time: finish_log.present? ? Time.at(finish_log.server_time - start_log.server_time).utc.strftime('%H:%M:%S') : '--:--:--'
       )
     end
   end
