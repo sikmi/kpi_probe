a5b60a2d267a316f901007151d67403b505d9100
KeisukeKitazumi <kitazumi@sikmi.com>
2023-11-09 14:16:57 +0900

    csvダウンロード実装

--- app/controllers/analyses/downloads_controller.rb
+++ app/controllers/analyses/downloads_controller.rb
@@ -1,6 +1,35 @@
 # frozen_string_literal: true
 
+require 'csv'
+
 module Analyses
   class DownloadsController < ApplicationController
+    def new
+      analyses = Analysis.search(search_params)
+      send_data generate_csv(analyses), filename: "analyses-#{Time.zone.now.strftime('%Y%m%d%H%M%S')}.csv"
+    end
+
+    private
+
+    def search_params
+      params.permit(:user_name, :process_name, :url, :start_date, :end_date, :hide_unfinished)
+    end
+
+    def generate_csv(analyses)
+      CSV.generate do |csv|
+        header = %w[開始日時 プロセス名 ユーザー名 URL 実行時間]
+        csv << header
+        analyses.each do |analysis|
+          column = [
+            l(analysis.started_at),
+            analysis.process_name,
+            analysis.user_name,
+            analysis.url,
+            analysis.time
+          ]
+          csv << column
+        end
+      end
+    end
   end
 end
