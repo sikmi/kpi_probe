73c09539eb0baaffee6bbe392de8770262cfa164
KeisukeKitazumi <kitazumi@sikmi.com>
2023-11-10 09:53:31 +0900

    ログを持っているユーザーだけ絞り込み

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,22 @@ class User < ApplicationRecord
   self.table_name = ENV['USER_TABLE'].blank? ? 'users' : ENV['USER_TABLE']
   self.primary_key = 'id'
 
+  has_many :visits, class_name: 'Matomo::Visit', foreign_key: 'user_id'
+
   def name
     if ENV['USER_NAME_ATTRIBUTE'].blank? || ENV['USER_NAME_ATTRIBUTE'] == 'name'
       super
     else
       send(ENV['USER_NAME_ATTRIBUTE'])
     end
+  end
+
+  def self.with_logs
+    categories = Matomo::Action.categories
+    start_logs = Matomo::LinkVisitAction.start_logs(categories)
+    users = start_logs.eager_load(visit: :user).map do |start_log|
+      start_log.visit.user
+    end
+    users.uniq
   end
 end
