09c5404d5411964ebf310b900ccb732adfe2f069
KeisukeKitazumi <kitazumi@sikmi.com>
2023-11-15 17:31:15 +0900

    所用時間順の並び替え変更

--- app/models/analysis.rb
+++ app/models/analysis.rb
@@ -14,18 +14,22 @@ class Analysis
       categories = Matomo::Action.categories.search_process_name(search_params[:process_name])
       start_logs = Matomo::LinkVisitAction.start_logs(categories).order_by_server_time(search_params[:order_by], search_params[:sort]).search_logs(search_params)
 
-      build_analyses(start_logs, search_params[:hide_unfinished]).compact
+      build_analyses(start_logs, search_params).compact
     end
 
     private
 
-    def build_analyses(start_logs, hide_unfinished)
-      start_logs.eager_load(visit: :user).map do |start_log|
+    def build_analyses(start_logs, search_params)
+      array = start_logs.eager_load(visit: :user).map do |start_log|
         finish_log = Matomo::LinkVisitAction.finish_log(start_log)
-        next if finish_log.nil? && hide_unfinished == '1'
+        next if finish_log.nil? && search_params[:hide_unfinished] == '1'
 
         build_one(start_log, finish_log)
       end
+
+      return array if search_params[:order_by] != 'time'
+
+      order_by_time(array, search_params[:sort])
     end
 
     def build_one(start_log, finish_log)
@@ -36,6 +40,14 @@ class Analysis
         url: start_log.action_name.name.split('::').first,
         time: finish_log.present? ? Time.at(finish_log.server_time - start_log.server_time).utc.strftime('%H:%M:%S') : '--:--:--'
       )
+    end
+
+    def order_by_time(array, sort)
+      if sort == 'asc'
+        array.sort_by(&:time)
+      elsif sort == 'desc'
+        array.sort_by(&:time).reverse
+      end
     end
   end
 end
