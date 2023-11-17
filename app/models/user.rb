52245e3c0f8439e22377bfd803697fda470ba703
KeisukeKitazumi <kitazumi@sikmi.com>
2023-11-17 16:30:50 +0900

    ユーザーの読み込みを改良

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,20 +6,13 @@ class User < ApplicationRecord
 
   has_many :visits, class_name: 'Matomo::Visit', foreign_key: 'user_id'
 
+  scope :with_logs, -> { joins(:visits).preload(:visits).uniq }
+
   def name
     if ENV['USER_NAME_ATTRIBUTE'].blank? || ENV['USER_NAME_ATTRIBUTE'] == 'name'
       super
     else
       send(ENV['USER_NAME_ATTRIBUTE'])
     end
-  end
-
-  def self.with_logs
-    categories = Matomo::Action.categories
-    start_logs = Matomo::LinkVisitAction.start_logs(categories)
-    users = start_logs.eager_load(visit: :user).map do |start_log|
-      start_log.visit.user
-    end
-    users.uniq
   end
 end
